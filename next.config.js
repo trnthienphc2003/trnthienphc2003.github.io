4dbc9d6df4f917a18f2791ccc51060c82dbc718f
trnthienphc2003 <21125090@student.hcmus.edu.vn>
2025-05-02 00:03:25 +0700

    Fix importing

--- next.config.js
+++ next.config.js
@@ -1,13 +1,29 @@
+/** @type {import('next').NextConfig} */
 const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/,
-    options: {
-    remarkPlugins: [require('remark-math')],
-    rehypePlugins: [require('rehype-katex')],
+    // options can be an async function returning the plugin array
+    options: async () => {
+        const [{ default: remarkMath }] = await Promise.all([
+            import('remark-math'),
+        ]);
+        const [{ default: rehypeKatex }] = await Promise.all([
+            import('rehype-katex'),
+        ]);
+        return {
+            remarkPlugins: [remarkMath],
+            rehypePlugins: [rehypeKatex],
+        };
     },
 });
   
 module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-    // later: basePath & assetPrefix for GitHub Pages…
+    pageExtensions: ['ts','tsx','md','mdx'],
+    basePath:
+    process.env.NODE_ENV === 'production'
+        ? '/username.github.io'
+        : '',
+    assetPrefix:
+    process.env.NODE_ENV === 'production'
+        ? '/username.github.io/'
+        : '',
 });
-  
